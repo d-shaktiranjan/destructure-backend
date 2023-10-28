16481292efe895396a349825f680d8a6e07e41a8
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2023-10-28 23:20:37 +0530

    feat: User model added

---
+++ src/models/User.model.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            minLength: 3,
+            required: true,
+        },
+        email: {
+            type: String,
+            minLength: 5,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export default model("User", userSchema);
