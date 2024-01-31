60a70179da3fd6945fdb86d8aa88530b53df6f6b
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2024-01-31 21:35:44 +0530

    chore: left hooks added

---
+++ .git_hooks/commitMessage.ts
@@ -0,0 +1,16 @@
+import { readFileSync } from "fs";
+
+const conventionalCommitMessageRegExp =
+    /^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test){1}(\([\w\-\.]+\))?(!)?: ([\w ])+([\s\S]*)/g;
+let exitCode = 0;
+const message = readFileSync("./.git/COMMIT_EDITMSG", "utf8");
+const isValid = conventionalCommitMessageRegExp.test(message);
+
+if (!isValid) {
+    console.log(
+        "Invalid commit message. Please follow the conventional commit format."
+    );
+    exitCode = 1;
+}
+
+process.exit(exitCode);
