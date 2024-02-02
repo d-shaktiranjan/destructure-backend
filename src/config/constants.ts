4dad1ddcf812b849d723b4d23ad173640da812fb
Shakti Ranjan Debata <51184377+d-shaktiranjan@users.noreply.github.com>
2024-02-02 23:47:32 +0530

    feat: implement google oAuth (#2)
    * feat: google oAuth implemented
    
    * feat: store user data in DB

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,11 +2,15 @@
 export const PORT = process.env.PORT || 8000;
 export const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017";
 export const DEBUG: boolean = process.env.DEBUG === "true";
+export const JWT_SECRET: string = process.env.JWT_SECRET || "";
+export const AUTH_TOKEN_EXPIRY: string = process.env.AUTH_TOKEN_EXPIRY || "3d";
 
 // google oAuth constants
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 export const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
+
+export const COOKIES_OPTIONS = { secure: true, httpOnly: true };
 
 export const APP_MESSAGES = {
     HELLO: "Hello from Express",
@@ -14,6 +18,12 @@ export const APP_MESSAGES = {
     ERROR: "Some error occurred",
     MISSING: " is missing",
 } as const;
+
+export enum AUTH_MESSAGES {
+    LOGIN = "Login success",
+    LOGOUT = "Logout success",
+    FAILED = "Login failed",
+}
 
 export enum DB_MESSAGES {
     CONNECTED = "DB Connected",
---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+import { OAuth2Client } from "google-auth-library";
+import {
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET,
+    GOOGLE_REDIRECT_URI,
+    AUTH_MESSAGES,
+    COOKIES_OPTIONS,
+} from "../config/constants";
+import asyncWrapper from "../middlewares/asyncWrap.middleware";
+import { errorResponse, successResponse } from "../utils/apiResponse.util";
+import User from "../models/User.model";
+
+const oAuth2Client = new OAuth2Client({
+    clientId: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    redirectUri: GOOGLE_REDIRECT_URI,
+});
+
+export const googleLogin = asyncWrapper(async (req: Request, res: Response) => {
+    const redirectUrl = oAuth2Client.generateAuthUrl({
+        scope: ["profile", "email"],
+    });
+    res.redirect(redirectUrl);
+});
+
+export const googleCallback = asyncWrapper(
+    async (req: Request, res: Response) => {
+        const { code } = req.query;
+
+        const { tokens } = await oAuth2Client.getToken(code as string);
+        oAuth2Client.setCredentials(tokens);
+
+        const user = await oAuth2Client.verifyIdToken({
+            idToken: tokens.id_token as string,
+            audience: GOOGLE_CLIENT_ID,
+        });
+        if (!user) return errorResponse(res, AUTH_MESSAGES.FAILED);
+        const userPayload = user.getPayload();
+
+        // store user in DB
+        let userObject = await User.findOne({ email: userPayload?.email });
+        if (userObject) {
+            userObject.name = userPayload?.name || "";
+            userObject.picture = userPayload?.picture;
+        } else {
+            userObject = new User({
+                name: userPayload?.name,
+                email: userPayload?.email,
+                picture: userPayload?.picture,
+            });
+        }
+        await userObject.save();
+        const jwt = userObject.generateAuthToken();
+        res.cookie("authToken", jwt, COOKIES_OPTIONS);
+
+        return successResponse(res, AUTH_MESSAGES.LOGIN, 200, userObject);
+    },
+);
+
+export const logout = (req: Request, res: Response) => {
+    res.clearCookie("authToken", COOKIES_OPTIONS);
+    return successResponse(res, AUTH_MESSAGES.LOGOUT);
+};
