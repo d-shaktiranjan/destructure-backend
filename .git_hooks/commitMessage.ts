7137ab407678e6eee9ac2f1a0ee0fa45fc8bd0e7
Shakti Ranjan Debata <51184377+d-shaktiranjan@users.noreply.github.com>
2024-03-14 21:17:02 +0530

    ci: format checker added & name added in action (#12)
    * chore: `prettier` format checker added
    
    * chore: allow backtick in commit message
    
    * ci: add format checker & name

--- .git_hooks/commitMessage.ts
+++ .git_hooks/commitMessage.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 
 const conventionalCommitMessageRegExp =
-    /^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test|merge|wip){1}(\([\w\-\.]+\))?(!)?: ([\w ])+([\s\S]*)/g;
+    /^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test|merge|wip){1}(\([\w\-\.]+\))?(!)?: \`?([\w ]+)\`?([\s\S]*)/g;
 let exitCode = 0;
 const message = readFileSync("./.git/COMMIT_EDITMSG", "utf8");
 const isValid = conventionalCommitMessageRegExp.test(message);
