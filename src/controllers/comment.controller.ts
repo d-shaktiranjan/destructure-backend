d31014feb9af39f7214af9c9111c148ae10c6c05
Shakti Ranjan Debata <51184377+d-shaktiranjan@users.noreply.github.com>
2024-05-10 23:05:53 +0530

    feat: `updateComment` route & controller added (#27)
    * feat: `isEdited` field added
    
    * feat: `updateComment` route & controller added
    
    * fix(comment): ownership checking
    
    * chore: bump version to `0.1.4`
    
    * docs: update CHANGELOG

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import { isValidObjectId } from "mongoose";
 
 // lib, model & lib
 import asyncWrapper from "../middlewares/asyncWrap.middleware";
@@ -42,11 +43,36 @@ export const removeComment = asyncWrapper(
         if (!comment) return errorResponse(res, COMMENT_MESSAGES.NOT_FOUND);
 
         // check ownership
-        if (comment.user !== req.user?._id)
+        if (String(comment.user) !== String(req.user?._id))
             return errorResponse(res, GENERIC_MESSAGES.NOT_ALLOWED, 401);
 
         // delete the comment
         await comment.deleteOne();
         return successResponse(res, COMMENT_MESSAGES.DELETED, 202);
+    },
+);
+
+export const updateComment = asyncWrapper(
+    async (req: AuthRequest, res: Response) => {
+        const { _id, content } = req.body;
+        nullChecker(res, { _id, content });
+
+        if (!isValidObjectId(_id))
+            return errorResponse(res, GENERIC_MESSAGES.INVALID_ID);
+
+        // fetch the comment
+        const comment = await Comment.findById(_id);
+        if (!comment) return errorResponse(res, COMMENT_MESSAGES.NOT_FOUND);
+
+        // check ownership
+        if (String(comment.user) !== String(req.user?._id))
+            return errorResponse(res, GENERIC_MESSAGES.NOT_ALLOWED, 401);
+
+        // update comment
+        comment.content = content as string;
+        comment.isEdited = true;
+        await comment.save();
+
+        return successResponse(res, COMMENT_MESSAGES.UPDATED, 202);
     },
 );
--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
 import { isAuthenticated } from "../middlewares/auth.middleware";
-import { addComment, removeComment } from "../controllers/comment.controller";
+import {
+    addComment,
+    removeComment,
+    updateComment,
+} from "../controllers/comment.controller";
 
 const router = Router();
 router.use(isAuthenticated);
 
-router.post("/add", addComment);
-router.delete("/remove", removeComment);
+router.route("/").post(addComment).delete(removeComment).put(updateComment);
 
 export default router;
