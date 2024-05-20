7d21bf608b8298808755e6f68471e3c0fca80f38
Shakti Ranjan Debata <51184377+d-shaktiranjan@users.noreply.github.com>
2024-05-20 23:42:09 +0530

    feat: `addReply` route & controller added (#35)
    * feat: `addReply` route & controller added
    
    * chore: bump version to `0.1.11`
    
    * docs: update CHANGELOG

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { allowBoth, isAuthenticated } from "../middlewares/auth.middleware";
 import {
     addComment,
+    addReply,
     getComments,
     getReplyList,
     removeComment,
@@ -19,6 +20,7 @@ protectedRouter
     .post(addComment)
     .delete(removeComment)
     .put(updateComment);
+protectedRouter.post("/reply", addReply);
 
 router.get("/", allowBoth, getComments);
 router.get("/reply", allowBoth, getReplyList);
