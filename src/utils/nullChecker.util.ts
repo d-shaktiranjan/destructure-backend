40df3a154e1559134fbc6e193667c42202f4c40f
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2024-07-20 19:12:05 +0530

    feat: improve `nullChecker`

--- src/utils/nullChecker.util.ts
+++ src/utils/nullChecker.util.ts
@@ -2,12 +2,11 @@ import { Response } from "express";
 import { APP_MESSAGES } from "../config/messages";
 import ApiError from "../libs/ApiError.lib";
 
-const nullChecker = (res: Response, elementObject: object): null | Response => {
+const nullChecker = (res: Response, elementObject: object) => {
     for (const [key, value] of Object.entries(elementObject)) {
         if (!value || value.trim() === "")
             throw new ApiError(key + APP_MESSAGES.MISSING);
     }
-    return null;
 };
 
 export default nullChecker;
