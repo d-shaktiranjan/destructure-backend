2a34283bd711a1a812847b2857cdcb78b383f79c
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2024-07-27 23:41:24 +0530

    feat: env variable added to controll log to store in Db

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import Logger from "../models/Logger.model";
+import { IS_STORE_LOG } from "../config/constants";
+console.log("IS_STORE_LOG", IS_STORE_LOG);
 
 const logger = (req: Request, res: Response, next: NextFunction) => {
     const originalSend = res.send.bind(res);
@@ -20,16 +22,18 @@ const logger = (req: Request, res: Response, next: NextFunction) => {
         console.log(`${method}- ${route} ${statusCode}`);
 
         // create log
-        const log = new Logger({
-            route,
-            statusCode,
-            method,
-            headers: req.headers,
-            query: req.query,
-            body: req.body,
-            response: JSON.parse((responseBody as string) || "{}"),
-        });
-        log.save();
+        if (IS_STORE_LOG) {
+            const log = new Logger({
+                route,
+                statusCode,
+                method,
+                headers: req.headers,
+                query: req.query,
+                body: req.body,
+                response: JSON.parse((responseBody as string) || "{}"),
+            });
+            log.save();
+        }
     });
 };
 
