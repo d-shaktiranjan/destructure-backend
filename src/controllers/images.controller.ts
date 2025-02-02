43b783cd5dd6b2ff6c85362498decb9308056278
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2025-02-02 18:10:41 +0530

    refactor: drop `204` responses (#70)
    * refactor: drop `204` responses
    
    * chore: update packages
    
    * chore: bump version to `1.0.3`

--- src/controllers/images.controller.ts
+++ src/controllers/images.controller.ts
@@ -2,11 +2,7 @@ import { readdir, unlink } from "fs/promises";
 import { Request, Response } from "express";
 
 import aw from "../middlewares/asyncWrap.middleware";
-import {
-    errorResponse,
-    noContentResponse,
-    successResponse,
-} from "../utils/apiResponse.util";
+import { errorResponse, successResponse } from "../utils/apiResponse.util";
 import { IMAGE_MESSAGES } from "../config/messages";
 import { ALLOWED_IMAGE_MIMETYPE } from "../config/constants";
 import { generateBase64 } from "../utils/blog.util";
@@ -17,7 +13,6 @@ export const imageList = aw(async (req: Request, res: Response) => {
     const dirList = (await readdir("public/images"))
         .filter((item) => item != ".gitkeep")
         .map((item) => host + item);
-    if (dirList.length === 0) return noContentResponse(res);
 
     return successResponse(res, IMAGE_MESSAGES.LIST_FETCHED, 200, dirList);
 });
