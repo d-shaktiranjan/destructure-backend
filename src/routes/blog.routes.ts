d4844442ba1533db090621169cb220edd27124ab
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2025-04-29 00:40:51 +0530

    refactor(blog): drop `blogStats` endpoint

--- src/routes/blog.routes.ts
+++ src/routes/blog.routes.ts
@@ -1,18 +1,12 @@
 import { Router } from "express";
 
 import { allowBoth } from "../middlewares/auth.middleware";
-import {
-    getBlogDetails,
-    getBlogList,
-    blogStats,
-} from "../controllers/blog.controller";
+import { getBlogDetails, getBlogList } from "../controllers/blog.controller";
 
 const router = Router();
 router.use(allowBoth);
 
 router.get("/", getBlogList);
 router.get("/:slug", getBlogDetails);
-
-router.get("/stats", blogStats);
 
 export default router;
