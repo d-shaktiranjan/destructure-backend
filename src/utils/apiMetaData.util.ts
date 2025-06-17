60858a6e61930ef9068e703d244eaca7483fc641
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2025-06-18 00:50:04 +0530

    feat: commit hash added in meta data

--- src/utils/apiMetaData.util.ts
+++ src/utils/apiMetaData.util.ts
@@ -1,4 +1,5 @@
 import { readFileSync } from "fs";
+import { logger } from "lorin";
 
 const getMetaDataFromPackage = () => {
     const packageJson = readFileSync("package.json", "utf8");
@@ -6,21 +7,32 @@ const getMetaDataFromPackage = () => {
     return { name, description, version };
 };
 
-const getRunningBranch = (): string => {
+const getCurrentGitBranchInfo = () => {
+    const data = {
+        runningBranch: "unknown",
+        commitHash: "unknown",
+    };
     try {
-        const file = readFileSync(".git/HEAD", "utf8");
-        return file.replace("ref: refs/heads/", "").replaceAll("\n", "");
-    } catch (error) {
-        if (error instanceof Error) return error.message;
-        return "Error";
+        const commitFile = readFileSync(".git/HEAD", "utf8");
+        const branchName = commitFile
+            .replace("ref: refs/heads/", "")
+            .replaceAll("\n", "");
+        const hashFile = readFileSync(".git/refs/heads/" + branchName, "utf8");
+        data.runningBranch = branchName;
+        data.commitHash = hashFile.replaceAll("\n", "");
+    } catch {
+        logger.error(
+            "Failed to read git branch or commit hash. Running in a non-git environment.",
+        );
     }
+    return data;
 };
 
 const apiMetaData = {
     ...getMetaDataFromPackage(),
     tag: process.env.TAG,
     host: "",
-    runningBranch: getRunningBranch(),
+    ...getCurrentGitBranchInfo(),
     documentation: "https://postman.destructure.in",
     license: "CC BY-NC-ND 4.0",
 };
