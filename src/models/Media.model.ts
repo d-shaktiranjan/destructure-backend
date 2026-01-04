59a8ee393c9cf2236a2a944660055056b01a043a
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2026-01-04 23:10:12 +0530

    feat(media): model added

---
+++ src/models/Media.model.ts
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+
+import { ALLOWED_MEDIA_MIMETYPE, MEDIA_TYPE } from "@/config/constants";
+import { MediaDocument } from "@/libs/Documents.lib";
+
+const mediaSchema = new Schema<MediaDocument>({
+    filePath: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    fileHash: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    type: {
+        type: String,
+        enum: MEDIA_TYPE,
+        required: true,
+    },
+    mimetype: {
+        type: String,
+        enum: ALLOWED_MEDIA_MIMETYPE,
+        required: true,
+    },
+    blurDataURL: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export default model<MediaDocument>("Media", mediaSchema);
