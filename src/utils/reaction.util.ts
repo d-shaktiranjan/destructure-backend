ab1f21e6e8ef81b3931b0b931d13b18a5861cdb7
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2026-01-16 23:12:59 +0530

    fix(reaction): correct variable name for reaction counts in getReactionsUtil

--- src/utils/reaction.util.ts
+++ src/utils/reaction.util.ts
@@ -6,7 +6,7 @@ export const getReactionsUtil = (
     req: AuthRequest,
     reactions: ReactionDocument[],
 ) => {
-    const counts = Object.values(REACTIONS).reduce(
+    const count = Object.values(REACTIONS).reduce(
         (acc, reaction) => {
             acc[reaction] = 0;
             return acc;
@@ -17,18 +17,15 @@ export const getReactionsUtil = (
     let givenReaction: ReactionDocument | null = null;
 
     for (const r of reactions) {
-        counts[r.reaction]++;
+        count[r.reaction]++;
         if (r.user.toString() === req.user?._id.toString()) {
             givenReaction = r;
         }
     }
 
     const data = {
-        own: {
-            isGiven: Boolean(givenReaction),
-            reaction: givenReaction?.reaction ?? null,
-        },
-        reactions: counts,
+        givenStatus: givenReaction?.reaction ?? null,
+        count,
     };
 
     return data;
